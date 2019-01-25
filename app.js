f6f9a5d8dbef101531ea306f8f59b81d63892296
HyeongJin Kim <qbwvwdp@icloud.com>
2019-01-25 11:29:15 +0900

    luis 추가

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ if(process.env.exec_env!="production"){
 // (추가)대화 로그 기록
 var log = require('./db/log');
 
+// (추가) - 측정을 위한 모듈 실행 코드 추가 
+const appInsights = require("applicationinsights");
+appInsights.setup(process.env.ApplicationInsightsKey);
+appInsights.start();
+
+
 // Setup Restify Server
 var server = restify.createServer(); // instance 생성
 server.listen(process.env.port || process.env.PORT || 3978, function () {  // listen 상태로 변환 (port 지정 - 대소문자 구문 해주기 때문에 port,PORT)
@@ -60,6 +66,13 @@ bot.use({
     }
 });
 
+// (추가) Create a recognizer that gets intents from LUIS, and add it to the bot
+const LuisModelUrl = process.env.LuisURL;
+console.log(`connect LUIS ${LuisModelUrl}`);
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+bot.recognizer(recognizer);
+
+
 // (추가) conversationUpdate 이벤트 핸들러
 bot.on('conversationUpdate', function (message) {
     if (message.membersAdded) {
@@ -102,3 +115,14 @@ bot.dialog('/', [
         }
     },
 ]);
+
+// (추가) 날씨문의 Dialog 추가
+// matches 영역에 직접 작성한 intent 명을 입력하시고, 응답 문구를 수정하세요.
+bot.dialog('날씨문의Dialog',
+    (session) => {
+        session.send('오늘 날씨는 온도 O도 습도 O%입니다.');
+        session.endDialog();
+    }
+).triggerAction({
+    matches: '날씨문의'
+});
