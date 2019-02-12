bfebbda8eb721c2ca16ffccf25047e5c434224e8
HyeongJin Kim <qbwvwdp@icloud.com>
2019-02-12 09:00:04 +0900

    중복제거Test01

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ var TempID = "TEST-ID005";
 // (수정)
 bot.dialog('/', [
     function (session,args) {
-        session.send(args.message.text);
+        session.send(args);
         session.send('안녕하세요. 제이드(Jaid)입니다.');  
         builder.Prompts.text(
                 session, 
